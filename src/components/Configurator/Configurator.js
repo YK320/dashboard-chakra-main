de9316dcccadd129b65a289a2b1f7bd114b7dba0
Yashoda Kawindi <150105261+YK320@users.noreply.github.com>
2025-03-01 18:57:29 +0530

    Add files via upload
    initial commit1

---
+++ src/components/Configurator/Configurator.js
@@ -0,0 +1,57 @@
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  Flex, Link,
+  Switch,
+  Text,
+  useColorMode,
+  useColorModeValue
+} from "@chakra-ui/react";
+import { HSeparator } from "components/Separator/Separator";
+import React, { useState } from "react";
+import GitHubButton from "react-github-btn";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
+
+export default function Configurator(props) {
+  const {
+    sidebarVariant,
+    setSidebarVariant,
+    secondary,
+    isOpen,
+    onClose,
+    fixed,
+    ...rest
+  } = props;
+  const [switched, setSwitched] = useState(props.isChecked);
+
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  let bgButton = useColorModeValue(
+    "linear-gradient(81.62deg, #313860 2.25%, #151928 79.87%)",
+    "white"
+  );
+  let colorButton = useColorModeValue("white", "gray.700");
+  const secondaryButtonBg = useColorModeValue("white", "transparent");
+  const secondaryButtonBorder = useColorModeValue("gray.700", "white");
+  const secondaryButtonColor = useColorModeValue("gray.700", "white");
+  const bgDrawer = useColorModeValue("white", "navy.800");
+  const settingsRef = React.useRef();
+  return (
+    <>
+      <Drawer
+        isOpen={props.isOpen}
+        onClose={props.onClose}
+        placement={document.documentElement.dir === "rtl" ? "left" : "right"}
+        finalFocusRef={settingsRef}
+        blockScrollOnMount={false}
+      >
+        
+      </Drawer>
+    </>
+  );
+}
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import { Flex, Link, List, ListItem, Text } from "@chakra-ui/react";
+import React from "react";
+
+export default function Footer(props) {
+  return (
+    <Flex
+      flexDirection={{
+        base: "column",
+        xl: "row",
+      }}
+      alignItems={{
+        base: "center",
+        xl: "start",
+      }}
+      justifyContent='space-between'
+      px='30px'
+      pb='20px'>
+     
+      
+    </Flex>
+  );
+}
